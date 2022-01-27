a0ddb44f2e21dc7916dff4d50face91e3251c26d
WairimuThuku <91134225+WairimuThuku@users.noreply.github.com>
2022-01-27 03:56:53 +0300

    Update quotes.ts

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,7 +1,7 @@
 export class Quotes {
     showQuote: boolean;
     dislike: any;
-      constructor(public index:number ,public name: string, public quote: string, public Author: string,public posted:Date) {
+      constructor(public index:number, public name: string, public quote: string, public Author: string, public posted:Date) {
           this.showQuote=false;
           
       }
